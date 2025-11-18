5caddb06dc3ba2040dbc38a33e00f6284f7c7b83
Brian Simmons <bsimmons01@gmail.com>
2025-11-18 11:50:26 -0700

    Initial Commit

---
+++ PlatziStoreREST/PlatziStoreRESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziStoreRESTApp.swift
+//  PlatziStoreREST
+//
+//  Created by Brian Simmons on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct PlatziStoreRESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
